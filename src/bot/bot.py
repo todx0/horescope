81bdf328bbb0cc068a5fe8d94fe54bf9893817ca
todx0 <todxsatan@gmail.com>
2024-12-07 15:55:47 +0100

    improve horoscope accuracy

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -8,10 +8,25 @@ from telegram.ext import (
 )
 from src.config.config import Config
 from src.api.gemini import Gemini
+from datetime import datetime
 
 
 class Bot:
     MENU = range(1)
+    ZODIAC_SIGNS = [
+        ("aries", "♈ Овен"),
+        ("taurus", "♉ Телец"),
+        ("gemini", "♊ Близнецы"),
+        ("cancer", "♋ Рак"),
+        ("leo", "♌ Лев"),
+        ("virgo", "♍ Дева"),
+        ("libra", "♎ Весы"),
+        ("scorpio", "♏ Скорпион"),
+        ("sagittarius", "♐ Стрелец"),
+        ("capricorn", "♑ Козерог"),
+        ("aquarius", "♒ Водолей"),
+        ("pisces", "♓ Рыбы"),
+    ]
 
     def __init__(self):
         self.application = Application.builder().token(Config.BOT_API_KEY).build()
@@ -22,24 +37,17 @@ class Bot:
         )
         self.application.add_handler(self.conv_handler)
         self.keyboard = [
-            [InlineKeyboardButton("♈ Овен", callback_data="aries")],
-            [InlineKeyboardButton("♉ Телец", callback_data="taurus")],
-            [InlineKeyboardButton("♊ Близнецы", callback_data="gemini")],
-            [InlineKeyboardButton("♋ Рак", callback_data="cancer")],
-            [InlineKeyboardButton("♌ Лев", callback_data="leo")],
-            [InlineKeyboardButton("♍ Дева", callback_data="virgo")],
-            [InlineKeyboardButton("♎ Весы", callback_data="libra")],
-            [InlineKeyboardButton("♏ Скорпион", callback_data="scorpio")],
-            [InlineKeyboardButton("♐ Стрелец", callback_data="sagittarius")],
-            [InlineKeyboardButton("♑ Козерог", callback_data="capricorn")],
-            [InlineKeyboardButton("♒ Водолей", callback_data="aquarius")],
-            [InlineKeyboardButton("♓ Рыбы", callback_data="pisces")],
+            [InlineKeyboardButton(translation, callback_data=sign)]
+            for sign, translation in self.ZODIAC_SIGNS
         ]
+
+    def run(self):
+        self.application.run_polling(allowed_updates=Update.ALL_TYPES)
 
     async def start(self, update: Update, context: CallbackContext) -> int:
         reply_markup = InlineKeyboardMarkup(self.keyboard)
         ai_response = await Gemini.text(
-            "Generate welcome message for horoscope bot and ask to choose an option below. Do not list zodiac signs"
+            "Generate welcome message for horoscope bot. Do not list zodiac signs. Cringe a lot."
         )
         await update.message.reply_text(ai_response, reply_markup=reply_markup)
         return self.MENU
@@ -52,32 +60,22 @@ class Bot:
         query = update.callback_query
         await query.answer()
 
-        if query.data in [
-            "aries",
-            "taurus",
-            "gemini",
-            "cancer",
-            "leo",
-            "virgo",
-            "libra",
-            "scorpio",
-            "sagittarius",
-            "capricorn",
-            "aquarius",
-            "pisces",
-        ]:
-            ai_response = await Gemini.text(
-                f"Generate long horoscope response for {query.data}"
-            )
-            await query.edit_message_text(text=ai_response)
-
-            file_path = f"images/{query.data}.png"
-            await self.application.bot.send_photo(
-                chat_id=query.from_user.id, photo=file_path
-            )
+        if query.data in [sign for sign, _ in self.ZODIAC_SIGNS]:
+            await self.edit_message_with_ai_generated_horoscope_result(query)
+            await self.send_zodiac_sign_image(query)
         else:
             await query.edit_message_text(text="Unknown option selected.")
             return self.MENU
 
-    def run(self):
-        self.application.run_polling(allowed_updates=Update.ALL_TYPES)
+    async def edit_message_with_ai_generated_horoscope_result(self, query):
+        current_date = datetime.now().strftime("%A, %d %B")
+        ai_response = await Gemini.text(
+            f"Generate long horoscope response. Zodiac sign: {query.data}, date: {current_date}"
+        )
+        await query.edit_message_text(text=ai_response)
+
+    async def send_zodiac_sign_image(self, query):
+        file_path = f"images/{query.data}.png"
+        await self.application.bot.send_photo(
+            chat_id=query.from_user.id, photo=file_path
+        )
